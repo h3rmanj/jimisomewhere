e392d672b8852039afaabfd649482095c00610dd
Herman Jensen <aa427@intility.no>
2019-09-09 14:22:08 +0200

    Added link to show from main page

--- src/components/Mobile/Main.js
+++ src/components/Mobile/Main.js
@@ -6,6 +6,7 @@ const Main = () =>
     <a href="https://ffm.to/ponyboy" target="_blank" rel="noopener noreferrer">MUSIC</a>
     <a href="https://www.youtube.com/channel/UCLs1oPB2HV3WmZSmnmPlKMg/videos" target="_blank" rel="noopener noreferrer">VIDEOS</a>
     <a href="https://everpress.com/forever-store" target="_blank" rel="noopener noreferrer">STORE</a>
+    <a href="https://www.ticketweb.uk/event/jimi-somewhere-the-camden-assembly-tickets/9882245" target="_blank" rel="noopener noreferrer">SHOWS</a>
     <Link to="/contact">CONTACT</Link>
   </div>
 
