8292ad882c61b6f2259d063cae78ec0e189e039b
Herman Jensen <aa427@intility.no>
2019-10-17 16:31:29 +0200

    update store link

--- src/components/Desktop/Main/index.js
+++ src/components/Desktop/Main/index.js
@@ -12,7 +12,7 @@ export default class extends Component {
 
 					<Link to="/music">MUSIC</Link>
 					<a href="https://www.youtube.com/channel/UCLs1oPB2HV3WmZSmnmPlKMg/videos" target="_blank" rel="noopener noreferrer">VIDEOS</a>
-					<a href="https://everpress.com/forever-store" target="_blank" rel="noopener noreferrer">STORE</a>
+					<a href="https://jimisomewhere.bigcartel.com/" target="_blank" rel="noopener noreferrer">STORE</a>
           <a href="https://www.ticketweb.uk/event/jimi-somewhere-the-camden-assembly-tickets/9882245" target="_blank" rel="noopener noreferrer">SHOWS</a>
 					<Link to="/contact">CONTACT</Link>
 				</div>
