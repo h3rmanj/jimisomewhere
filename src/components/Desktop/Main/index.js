abf84ceeddafef88d3cb940b1c07c0eaa948943f
Fredrik Holanger / Intility AS <fredrik.holanger@intility.no>
2021-03-26 21:46:55 +0100

    adds background, adds new font

--- src/components/Desktop/Main/index.js
+++ src/components/Desktop/Main/index.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Video from './Video';
 import './styles.css';
+import jimi from '../../../img/main_background.png'
 
 export default class extends Component {
 	render = () =>
 		<div>
-			<Video />
+		
 			<div className="Main">
+			<img className="bgImage" src={jimi}/>
 				<div className="LinkWrapper">
-
+		
 					<Link to="/music">MUSIC</Link>
 					<a href="https://www.youtube.com/channel/UCLs1oPB2HV3WmZSmnmPlKMg/videos" target="_blank" rel="noopener noreferrer">VIDEOS</a>
 					<a href="https://jimisomewhere.bigcartel.com/" target="_blank" rel="noopener noreferrer">STORE</a>
@@ -17,5 +19,7 @@ export default class extends Component {
 					<Link to="/contact">CONTACT</Link>
 				</div>
 			</div>
+			
+				
 		</div>
 }
