acd4314349ea8fa92e343e9cab35064f7380bf47
Fredrik Holanger / Intility AS <fredrik.holanger@intility.no>
2021-03-27 17:13:31 +0100

    changes size of nav links

--- src/components/Desktop/Main/index.js
+++ src/components/Desktop/Main/index.js
@@ -12,11 +12,11 @@ export default class extends Component {
 			<img className="bgImage" src={jimi}/>
 				<div className="LinkWrapper">
 		
-					<Link to="/music">MUSIC</Link>
-					<a href="https://www.youtube.com/channel/UCLs1oPB2HV3WmZSmnmPlKMg/videos" target="_blank" rel="noopener noreferrer">VIDEOS</a>
-					<a href="https://jimisomewhere.bigcartel.com/" target="_blank" rel="noopener noreferrer">STORE</a>
-          <a href="https://www.ticketweb.uk/event/jimi-somewhere-the-camden-assembly-tickets/9882245" target="_blank" rel="noopener noreferrer">SHOWS</a>
-					<Link to="/contact">CONTACT</Link>
+					<Link to="/music">Music</Link>
+					<a href="https://www.youtube.com/channel/UCLs1oPB2HV3WmZSmnmPlKMg/videos" target="_blank" rel="noopener noreferrer">Videos</a>
+					<a href="https://jimisomewhere.bigcartel.com/" target="_blank" rel="noopener noreferrer">Store</a>
+          <a href="https://www.ticketweb.uk/event/jimi-somewhere-the-camden-assembly-tickets/9882245" target="_blank" rel="noopener noreferrer">Shows</a>
+					<Link to="/contacts">Contact</Link>
 				</div>
 			</div>
 			
