4b427a0280196d4c832ee25c4b6c618bddf749a2
Fredrik Holanger / Intility AS <fredrik.holanger@intility.no>
2021-05-28 20:15:23 +0200

    adds new shows and links

--- src/components/Desktop/Shows/ShowsFields.js
+++ src/components/Desktop/Shows/ShowsFields.js
@@ -5,7 +5,29 @@ import React from 'react';
 const ShowsFields = () => (
     <div className="ShowsFields" >
     <h1 className="ShowsHeader">Shows</h1>
-<div className="ShowsField"><a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.ticketmaster.dk%2Fevent%2Fjimi-somewhere-live-in-copenhagen-2021-billetter%2F496045%3F_ga%3D2.135745848.807693378.1616876856-1801563635.1616876856%26fbclid%3DIwAR0MmlhyrfBdJqEVc-fOTCAmhgFauu8YsXBqHPJPyI4eacrP53EuzP4uKXM&h=AT2W6klO82Nzrugtx-36uhzZ81C6fPd3MhZoRLs_IttbS88aGGz1IG1YAhSyJhF8rMdCqANQ1pZJ8TVASt_VKIMIFFyM0PbfLNHZfjoM6yXyHhJWwcZa_TMQjiEycH-Nr5w">09.19.20 - Hotel Cecil - København, Denmark</a></div>
+<div className="ShowsField"><a href="https://www.ticketmaster.no/event/jimi-somewhere-billetter/656075">23.06.21 - Oslo, Norway</a></div>
+<div className="ShowsField"><a href="">29.04.22 - Madrid, Spain</a></div>
+<div className="ShowsField"><a href="">30.04.22 - Barcelona, Spain</a></div>
+<div className="ShowsField"><a href="">01.05.22 - Brussels, Belgium</a></div>
+<div className="ShowsField"><a href="http://link.dice.fm/OvqfQfRJCgb">02.05.22 - Paris, France</a></div>
+<div className="ShowsField"><a href="https://www.seetickets.com/event/jimi-somewhere/colours/1860004">03.05.22 - London, United Kingdom</a></div>
+<div className="ShowsField"><a href="http://hdfst.uk/E64776">05.05.22 - Bristol, United Kingdom</a></div>
+<div className="ShowsField"><a href="https://www.seetickets.com/event/jimi-somewhere/yes-the-basement-/1859652">06.05.22 - Manchester, United Kingdom</a></div>
+<div className="ShowsField"><a href="https://link.dice.fm/AHH8vhuJzgb">07.05.22 - Leeds, United Kingdom</a></div>
+<div className="ShowsField"><a href="https://432p.seetickets.com/event/jimi-somewhere/cca/1883032">08.05.22 - Glasgow, United  Kingdom</a></div>
+<div className="ShowsField"><a href="http://bit.ly/JimiSomewhereNCL">09.05.22 - Newscastle, United Kingdom</a></div>
+<div className="ShowsField"><a href="https://www.skiddle.com/e/35827633">10.05.22 - Liverpool, United Kingdom</a></div>
+<div className="ShowsField"><a href="https://www.seetickets.com/event/jimi-somewhere/mama-roux-s/1859642">11.05.22 - Birmingham, United Kingdom</a></div>
+<div className="ShowsField"><a href="">12.05.22 - Brighton, United Kingdom</a></div>
+<div className="ShowsField"><a href="https://link.dice.fm/tnhfNXmZAgb">14.05.22 - Milan, Italy</a></div>
+<div className="ShowsField"><a href="https://www.starticket.ch/de/tickets/jimi-somewhere-no-20220515-2000-papiersaal-zurich">15.05.22 - Zurich, Switzerland</a></div>
+<div className="ShowsField"><a href="https://akvariumklub.hu/?post_type=programs-cpt&p=12900&preview=true">17.05.22 - Budapest, Hungary</a></div>
+<div className="ShowsField"><a href="https://www.oeticket.com/event/13768119/?affiliate=EOE">18.05.22 - Vienna, Austria</a></div>
+<div className="ShowsField"><a href="https://www.universe.com/events/jimi-somewhere-euro66-tickets-YZQM84">20.05.22 - Berlin, Germany</a></div>
+<div className="ShowsField"><a href="https://www.ticketmaster.nl/event/269805?brand=nl_melkweg">21.05.22 - Amsterdam, Netherlands</a></div>
+<div className="ShowsField"><a href="https://www.ticketmaster.dk/event/VEI2305K?language=da-dk&camefrom=vegadk&brand=dk_vega">23.05.22 - Copenhagen, Denmark</a></div>
+
+
 </div>
 );
 
--- src/components/Mobile/Main.js
+++ src/components/Mobile/Main.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Main = () =>
   <div className="MobileMain">
-    <a href="https://ffm.to/ponyboy" target="_blank" rel="noopener noreferrer">Music</a>
+    <a href="https://ffm.to/ngcs" target="_blank" rel="noopener noreferrer">Music</a>
     <a href="https://www.youtube.com/channel/UCLs1oPB2HV3WmZSmnmPlKMg/videos" target="_blank" rel="noopener noreferrer">Videos</a>
     <a href="https://thehyv.shop/collections/jimi-somewhere" target="_blank" rel="noopener noreferrer">Store</a>
     <Link to="shows">Shows</Link>
