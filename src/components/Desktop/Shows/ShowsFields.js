a942378a206849dd5e942671be6cbcd11027b4f3
Fredrik Holanger <fredrik.holanger@intility.no>
2022-04-12 20:11:05 +0200

    Updated tour dates

--- src/components/Desktop/Shows/ShowsFields.js
+++ src/components/Desktop/Shows/ShowsFields.js
@@ -5,26 +5,19 @@ import React from 'react';
 const ShowsFields = () => (
     <div className="ShowsFields" >
     <h1 className="ShowsHeader">Shows</h1>
-<div className="ShowsField"><a href="https://www.ticketmaster.no/event/jimi-somewhere-billetter/656075">23.06.21 - Oslo, Norway</a></div>
-<div className="ShowsField"><a href="">29.04.22 - Madrid, Spain</a></div>
-<div className="ShowsField"><a href="">30.04.22 - Barcelona, Spain</a></div>
-<div className="ShowsField"><a href="">01.05.22 - Brussels, Belgium</a></div>
+
 <div className="ShowsField"><a href="http://link.dice.fm/OvqfQfRJCgb">02.05.22 - Paris, France</a></div>
 <div className="ShowsField"><a href="https://www.seetickets.com/event/jimi-somewhere/colours/1860004">03.05.22 - London, United Kingdom</a></div>
 <div className="ShowsField"><a href="http://hdfst.uk/E64776">05.05.22 - Bristol, United Kingdom</a></div>
 <div className="ShowsField"><a href="https://www.seetickets.com/event/jimi-somewhere/yes-the-basement-/1859652">06.05.22 - Manchester, United Kingdom</a></div>
 <div className="ShowsField"><a href="https://link.dice.fm/AHH8vhuJzgb">07.05.22 - Leeds, United Kingdom</a></div>
-<div className="ShowsField"><a href="https://432p.seetickets.com/event/jimi-somewhere/cca/1883032">08.05.22 - Glasgow, United  Kingdom</a></div>
 <div className="ShowsField"><a href="http://bit.ly/JimiSomewhereNCL">09.05.22 - Newscastle, United Kingdom</a></div>
-<div className="ShowsField"><a href="https://www.skiddle.com/e/35827633">10.05.22 - Liverpool, United Kingdom</a></div>
-<div className="ShowsField"><a href="https://www.seetickets.com/event/jimi-somewhere/mama-roux-s/1859642">11.05.22 - Birmingham, United Kingdom</a></div>
-<div className="ShowsField"><a href="">12.05.22 - Brighton, United Kingdom</a></div>
-<div className="ShowsField"><a href="https://link.dice.fm/tnhfNXmZAgb">14.05.22 - Milan, Italy</a></div>
-<div className="ShowsField"><a href="https://www.starticket.ch/de/tickets/jimi-somewhere-no-20220515-2000-papiersaal-zurich">15.05.22 - Zurich, Switzerland</a></div>
-<div className="ShowsField"><a href="https://akvariumklub.hu/?post_type=programs-cpt&p=12900&preview=true">17.05.22 - Budapest, Hungary</a></div>
-<div className="ShowsField"><a href="https://www.oeticket.com/event/13768119/?affiliate=EOE">18.05.22 - Vienna, Austria</a></div>
+<div className="ShowsField"><a href="https://akvariumklub.hu/?post_type=programs-cpt&p=12900&preview=true">18.05.22 - Budapest, Hungary</a></div>
+
 <div className="ShowsField"><a href="https://www.universe.com/events/jimi-somewhere-euro66-tickets-YZQM84">20.05.22 - Berlin, Germany</a></div>
+
 <div className="ShowsField"><a href="https://www.ticketmaster.nl/event/269805?brand=nl_melkweg">21.05.22 - Amsterdam, Netherlands</a></div>
+        <div className="ShowsField"><a href="https://botanique.be/nl/concert/jimi-somewhere-eugene-2022">22.05.22 - Brussels, Belgium</a></div>
 <div className="ShowsField"><a href="https://www.ticketmaster.dk/event/VEI2305K?language=da-dk&camefrom=vegadk&brand=dk_vega">23.05.22 - Copenhagen, Denmark</a></div>
 
 
